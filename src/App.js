acd5e01d28cb24c90adcd1a75fb6dab9c711ef0b
BACHALI.RAMBABU <rambabubachali@gmail.com>
2023-02-18 12:23:13 +0530

    Login Ui Created By Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome To React</h1>.
+      <h2>Steps create repository</h2>
+      <div>
+      <form>
+  <div className="mb-3">
+    <label for="username" className="form-label">Username</label>
+    <input type="text" className="form-control" id="username" name="username" placeholder="Enter your username"/>
+  </div>
+  <div className="mb-3">
+    <label for="password" className="form-label">Password</label>
+    <input type="password" className="form-control" id="password" name="password" placeholder="Enter your password"/>
+  </div>
+  <button type="submit" className="btn btn-primary">Login</button>
+</form>
+      </div>
     </div>
   );
 }
